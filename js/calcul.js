0fbd85a135ec9dcf1e408e8faa0e098e23fc5369
John <raul.delianu@epitech.eu>
2020-01-27 23:33:09 +0100

    fixtures: auto compt workhours anf coffee cup

--- js/calcul.js
+++
@@ -1,9 +0,0 @@
-function age(date) {
-    var diff = Date. now() - date. getTime();
-    var age = new Date(diff);
-    var monAge = Math. abs(age. getUTCFullYear() - 1970);
-    $('.myAge').replaceWith("<h2>Bonjour,  je m’appelle <br> <span> Delianu Raul </span> et j’ai " + monAge + " ans.</h2>")
-}
-$(function () {
-    age(new Date(1994, 11, 17));
-});
